b52f617c7376e42f2746f73ea37a77b3a0d0330c
milanperera1993 <milanperera1993@gmail.com>
2025-01-06 13:27:45 +0800

    adding routes for; getAllBooks, getBookById, updateBook, deteleBook

--- src/books/book.controller.js
+++ src/books/book.controller.js
@@ -1,5 +1,6 @@
 const Book = require("./book.model");
 
+// create book
 const createBook = async (req, res) => {
   try {
     const newBook = await Book({ ...req.body });
@@ -13,6 +14,72 @@ const createBook = async (req, res) => {
   }
 };
 
+// get all book
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find().sort({ createdAt: -1 });
+    res.status(200).send({ books });
+  } catch (error) {
+    console.error("Error fetching books", error);
+    res.status(500).send({ message: "Failed to fetch books" });
+  }
+};
+
+// get a book by id
+const getBookById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (!book) {
+      return res.status(404).send({ message: "Book not found" });
+    }
+    res.status(200).send(book);
+  } catch (error) {
+    console.error("Error fetching book", error);
+    res.status(500).send({ message: "Failed to fetch book" });
+  }
+};
+
+// update book
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedBook = await Book.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    if (!updatedBook) {
+      return res.status(404).send({ message: "Book not found" });
+    }
+    res
+      .status(200)
+      .send({ message: "Book updated successfully!", book: updateBook });
+  } catch (error) {
+    console.error("Error updating book", error);
+    res.status(500).send({ message: "Failed to update book" });
+  }
+};
+
+// delete book
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedBook = await Book.findByIdAndDelete(id);
+    if (!deletedBook) {
+      return res.status(404).send({ message: "Book not found!" });
+    }
+    res
+      .status(200)
+      .send({ message: "Book deleted successfully!", book: deletedBook});
+  } catch (error) {
+    console.error("Error deleting book", error);
+    res.status(500).send({ message: "Failed deleting book." });
+  }
+};
+
 module.exports = {
   createBook,
+  getAllBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
 };
--- src/books/book.route.js
+++ src/books/book.route.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
-const { createBook } = require("./book.controller");
+const { createBook, getAllBooks, getBookById, updateBook, deleteBook } = require("./book.controller");
 const router = express.Router();
 
 // post a book
 router.post("/create-book", createBook);
-
 // get books
+router.get("/", getAllBooks);
+// get a book by id
+router.get("/:id", getBookById);
+// update book
+router.put("/edit/:id", updateBook)
+// delete book
+router.delete("/:id", deleteBook)
 
 module.exports = router;
